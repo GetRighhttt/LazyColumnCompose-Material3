31fa6eb4db5d3366f12a949888a9d17665c0e9c0
Stefan Bayne <stefanbusiness95@gmail.com>
2023-09-20 14:11:38 -0400

    finished lazy column example.

--- app/src/main/java/com/example/lazycolumnmat3compose/MainActivity.kt
+++ app/src/main/java/com/example/lazycolumnmat3compose/MainActivity.kt
@@ -3,18 +3,32 @@ package com.example.lazycolumnmat3compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lazycolumnmat3compose.ui.theme.LazyColumnMat3ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val characterList = characterNames.map {
+            Category(
+                firstInitial = it.key.toString(),
+                items = it.value
+            )
+        }
         setContent {
             LazyColumnMat3ComposeTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +36,64 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CategorizedLazyColumn(categories = characterList)
                 }
             }
         }
     }
 }
 
+data class Category(
+    val firstInitial: String,
+    val items: List<String>
+)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun CategoryHeader(
+    text: String,
+    modifier: Modifier = Modifier
+) {
     Text(
-        text = "Hello $name!",
+        text = text,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
         modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(16.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LazyColumnMat3ComposeTheme {
-        Greeting("Android")
+private fun CategoryItem(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Light,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun CategorizedLazyColumn(
+    categories: List<Category>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier) {
+        categories.forEach { category ->
+            stickyHeader {
+                CategoryHeader(category.firstInitial)
+            }
+            items(category.items) { text ->
+                CategoryItem(text)
+            }
+        }
     }
 }
